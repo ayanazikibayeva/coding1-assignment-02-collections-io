38eb288d13528584a6b5fc079b3c699a2a8413ee
Ayana Zikibayeva <zikibayeva_ayana@student.ceu.edu>
2026-09-23 20:17:56 +0500

    Prediction of print and comments

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" # this defines the variable drink and gives it value "tea"
+sales = {"tea": 3, "coffee": 5} # this defines sales variable in which "tea" has a value 3 and "coffee" has a value 5
+sold = sales[drink] # this defines sold variable and assigns the drink variable to the sales
+sold = sold + 1 # sold variable is increased by 1
+print(drink) # prints the value of drink that is "tea" which is defined in the first line
+print(sold) # prints the value of sold tea which is gonna be 4 because in sales tea has a value of 3 and we increased it by 1
